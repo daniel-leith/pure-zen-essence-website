78d82d66944c82ccde1228472f4828b34b9a1fdf
Daniel Leith <danielleith3@gmail.com>
2025-01-18 22:20:53 -0500

    Update form styles

--- dist/javascript/main.js
+++ dist/javascript/main.js
@@ -1,5 +1,5 @@
 gsap.set("#cta-container", { y: 200, opacity: 0 });
 
 window.onload = () => {
-  gsap.to("#cta-container", { y: -25, opacity: 1, duration: 1.25 });
+  gsap.to("#cta-container", { y: -30, opacity: 1, duration: 1.25 });
 };
