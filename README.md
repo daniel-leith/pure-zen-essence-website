24d98506c93ee56cf0bac85c4c274394b9918391
Daniel Leith <danielleith3@gmail.com>
2025-02-01 20:13:31 -0500

    Remove unused GSAP files and clean up README and robots.txt

--- README.md
+++
@@ -1,2 +0,0 @@
-<!-- TODO: Check code with ChatGPT after finishing -->
-<!-- TODO: Add a site description -->
--- dist/javascript/gsap.js
+++ dist/javascript/gsap.js
@@ -1,5 +1,1 @@
-gsap.set("#cta-container", { y: 200, opacity: 0 });
 
-window.onload = () => {
-  gsap.to("#cta-container", { y: -30, opacity: 1, duration: 1.25 });
-};
