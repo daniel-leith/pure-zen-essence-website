b5cb470a6f6eda21c7e3854d4d507813f6800de7
Daniel Leith <danielleith3@gmail.com>
2025-03-26 18:11:49 -0500

    Style cards and improve HTML/SCSS/JS structure

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,6 @@
 const sectionLinks = document.querySelectorAll(".primary-navbar__section-link");
 const cardButtons = document.querySelectorAll(".card__button");
+const cardCloseButtons = document.querySelectorAll(".card__close");
 
 sectionLinks.forEach((link) => {
   // change the navbar link colour when clicked to
@@ -19,6 +20,16 @@ cardButtons.forEach((button) => {
 
   button.addEventListener("click", () => {
     console.log("Card Flipped");
-    card.classList.toggle("flipped");
+    card.classList.add("flipped");
+  });
+});
+
+cardCloseButtons.forEach((button) => {
+  // flip the card when the close button is clicked
+  const card = button.parentElement.parentElement;
+
+  button.addEventListener("click", () => {
+    console.log("Card Flipped");
+    card.classList.remove("flipped");
   });
 });
