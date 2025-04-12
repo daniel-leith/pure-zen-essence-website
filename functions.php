7ee8db0101ba5dfb178a67c2621fe1fb82e89326
Daniel Leith <danielleith3@gmail.com>
2025-04-12 08:49:15 -0500

    Create authorise function

--- functions.php
+++ functions.php
@@ -13,3 +13,10 @@ function urlIs($url)
 {
   return $_SERVER["REQUEST_URI"] === $url;
 }
+
+function authorise($condition, $status = Response::FORBIDDEN)
+{
+  if (!$condition) {
+    abort($status);
+  }
+}
